b812a06e4a677472e28d0fabe7b031c85b111e0b
Alexandr <sashausik56@gmail.com>
2021-01-07 01:59:10 +0700

    Commit with first menu,not all menu.

--- main.py
+++ main.py
@@ -1,16 +1,107 @@
-# This is a sample Python script.
+import time
+import os
+import sys
+import pygame
+import random
+FPS = 144
+WIDTH = 998
+HEIGHT = 1200
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def terminate():
+    pygame.quit()
+    sys.exit()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def start_screen():
 
 
-# Press the green button in the gutter to run the script.
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+class Mountain(pygame.sprite.Sprite):
+    image = load_image("mountains.png")
+
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.image = Mountain.image
+        self.rect = self.image.get_rect()
+        # вычисляем маску для эффективного сравнения
+        self.mask = pygame.mask.from_surface(self.image)
+        # располагаем горы внизу
+        self.rect.bottom = HEIGHT
+
+
+class Landing(pygame.sprite.Sprite):
+    image = load_image("pt.png")
+
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.image = Landing.image
+        self.rect = self.image.get_rect()
+        # вычисляем маску для эффективного сравнения
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self):
+        # если ещё в небе
+        if not pygame.sprite.collide_mask(self, mountain):
+            self.rect = self.rect.move(0, 1)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pygame.init()
+    pygame.display.set_caption('Свой курсор мыши')
+    size = WIDTH , HEIGHT
+    screen = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    running = True
+    all_sprites = pygame.sprite.Group()
+    horizontal_borders = pygame.sprite.Group()
+    vertical_borders = pygame.sprite.Group()
+    y = 0
+    # создадим спрайт
+    # добавим спрайт в группу
+    mountain = Mountain()
+    n = 1
+    while n == 1:
+        start_screen()
+    while running:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                y = 1
+                break
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                Landing(event.pos)
+        if y == 1:
+            break
+        all_sprites.draw(screen)
+        all_sprites.update()
+        pygame.display.flip()
+        clock.tick(144)
+    pygame.quit()
